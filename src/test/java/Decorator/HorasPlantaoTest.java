d1ecc7ea4c92e2a743928da01e2675d92d82c496
Victoria <vicoliveira116@gmail.com>
2023-03-31 15:27:52 -0300

    Concluido

---
+++ src/test/java/Decorator/HorasPlantaoTest.java
@@ -0,0 +1,10 @@
+package Decorator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HorasPlantaoTest {
+
+    @org.junit.jupiter.api.Test
+    void getPercentualCargaHoraria() {
+    }
+}
